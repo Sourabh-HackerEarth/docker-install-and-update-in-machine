53383c07922bbceec99c1ab9967396fd981f07f1
sourabhbhandari <bhandari.sourabh372@gmail.com>
2021-03-04 12:41:50 +0530

    Added Colour and Correct the status

--- update-and-install.sh
+++ update-and-install.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
-echo "Starting On New Machine"
+echo -e "\033[31;7Script Started on This Machine\e[0m";
 
 echo -e "------------------------------------------------------------------------------------------------------------------------"
 
-echo -e "Updating The machine"
+echo -e "\033[31;7mUpdating The Machine\e[0m";
 
 sudo apt update
 
-echo -e "Installing CA Certificates"
+echo -e "\033[31;7mInstalling CA Certificates\e[0m";
+
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-echo -e "Add the GPG key for the official Docker repository to your system..."
+echo -e "\033[31;7mAdd the GPG key for the official Docker repository to your system...\e[0m";
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo -e "Docker repository to APT sources"
+echo -e "\033[31;7mDocker repository to APT sources\e[0m";
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-echo -e "Updating The machine again"
+echo -e "\033[31;7mUpdating The Machine AGAIN\e[0m";
 
 sudo apt update
 
-echo -e "Docker repo instead of the default Ubuntu repo"
+echo -e "\033[31;7mDocker repo instead of the default Ubuntu repo\e[0m";
 
 sudo apt-cache policy docker-ce
 
-echo -e ""
+echo -e "\033[31;7mInstalling Docker Finally\e[0m";
 
 sudo apt install docker-ce
 
-echo -e "Updating The machine"
+echo -e "\033[31;7mMachine Updated and Docker Installed\e[0m";
 
-status =$(sudo systemctl status docker)
-echo "Docker Status $status"
+echo -e "\033[31;7mDocker Status is : \e[0m";
+
+sudo systemctl status docker)
+
 
